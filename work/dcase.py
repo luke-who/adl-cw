c4a62adcb04ae10b6be0318770d71b49c6e84459
luke-who <luke.z0423@gmail.com>
2021-12-04 00:51:56 +0000

    create data directory & move ADL_DCASE_DATA -> data, add additional helper code for data processing

--- work/dcase.py
+++ work/dcase.py
@@ -7,57 +7,9 @@ import argparse
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from ..data import dataset
 
-class DCASE(Dataset):
-    def __init__(self, root_dir: str, clip_duration: int):
-        self._root_dir = Path(root_dir)
-        self._labels = pd.read_csv((self._root_dir / 'labels.csv'), names=['file', 'label'])
-        # print(self._labels.label.astype('category'))
-        self._labels['label'] = self._labels.label.astype('category').cat.codes.astype('int') #create categorical labels
-        self._clip_duration = clip_duration
-        self._total_duration = 30 #DCASE audio length is 30s
 
-        self._data_len = len(self._labels)
-
-    def __getitem__(self, index):
-        #reading spectrograms
-        filename, label = self._labels.iloc[index]
-        filepath = self._root_dir / 'audio'/ filename
-        spec = torch.from_numpy(np.load(filepath))
-
-        #splitting spec
-        spec = self.__trim__(spec)
-        return spec, label
-
-    def __trim__(self, spec: torch.Tensor) -> torch.Tensor:
-        """
-        Trims spectrogram into multiple clips of length specified in self._num_clips
-        :param spec: tensor containing spectrogram of full audio signal of shape [1, 60, 1501]
-        :return: tensor containing stacked spectrograms of shape [num_clips, 60, clip_length] ([10, 60, 150] with 3s clips)
-        """
-        time_steps = spec.size(-1)
-        self._num_clips = self._total_duration // self._clip_duration
-        time_interval = int(time_steps // self._num_clips)
-        all_clips = []
-        for clip_idx in range(self._num_clips):
-            start = clip_idx * time_interval
-            end = start + time_interval
-            spec_clip = spec[:, start:end]
-            #spec_clip = torch.squeeze(spec_clip)
-            all_clips.append(spec_clip)
-
-        specs = torch.stack(all_clips)
-        return specs
-
-    def get_num_clips(self) -> int:
-        """
-        Gets number of clips the raw audio has been split into
-        :return: self._num_clips of type int
-        """
-        return self._num_clips
-
-    def __len__(self):
-        return self._data_len
 
 class AudioCNN(nn.Module):
     def __init__(self, args, in_channels=1):
@@ -210,8 +162,8 @@ def main(args):
     print(f"Using {device} device.")      
 
     # Load datasets.
-    training_data = DCASE(Path(args.dataset_root) / "development", 3)
-    test_data = DCASE(Path(args.dataset_root) / "evaluation" , 3)
+    training_data = dataset.DCASE(Path(args.dataset_root) / "development", 3)
+    test_data = dataset.DCASE(Path(args.dataset_root) / "evaluation" , 3)
 
     # Create data loaders.
     train_dataloader = DataLoader(
@@ -244,7 +196,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch-size", default=64, type=int)
-    parser.add_argument("--dataset-root", default="../ADL_DCASE_DATA")
+    parser.add_argument("--dataset-root", default="../data/ADL_DCASE_DATA")
     parser.add_argument("--log-dir", default=Path("logs"), type=Path)
     parser.add_argument("--metric-frequency", default=1, type=int)
     parser.add_argument("--epochs", default=20, type=int)
