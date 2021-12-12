f5fcd3e4c9849977235928f399aaebca2c4d8701
Ben <bmj6000@virginmedia.com>
2021-12-12 14:53:24 +0000

    minor fix

--- work/dcase.py
+++ work/dcase.py
@@ -262,9 +262,9 @@ def get_summary_writer_log_dir(args: argparse.Namespace, command_prefix = "") ->
         f"CNN_bn_"
         f"{args.prefix[1:-1]}"
         f"bs={args.batch_size}_"
-        f"lr={args.learning_rate}_" +
+        f"lr={args.learning_rate}_"
         f"valid_frequency={args.valid_frequency}_"
-        f"max_worsen_streak={args.max_worsen_streak}_"
+        f"max_worsen_streak={args.max_worsen_streak}_" +
         (f"dropout={args.dropout}_" if args.dropout!=0 else "") +
         f"run_"
     )
