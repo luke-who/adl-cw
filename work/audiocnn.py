2f66b1750d3556fef2eeb0e44abfefdcf12de321
Ben <bmj6000@virginmedia.com>
2021-12-17 07:33:07 +0000

    Improvement: another dense layer.

--- work/audiocnn.py
+++ work/audiocnn.py
@@ -43,13 +43,13 @@ class AudioCNN(nn.Module):
             
             
             nn.Flatten(),
-            # nn.Dropout(p=args.dropout),
-            # nn.Linear(3072,1000),
-            # nn.BatchNorm1d(1000),
-            # nn.ReLU(),
+            nn.Dropout(p=args.dropout),
+            nn.Linear(3072,1000),
+            nn.BatchNorm1d(1000),
+            nn.ReLU(),
             
             nn.Dropout(p=args.dropout),
-            nn.Linear(3072, categories)
+            nn.Linear(1000, categories)
         )
 
     def forward(self, x):
